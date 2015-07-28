c14e5d62d60a0da85e6210d932ebe80549f6d2ea
nawazish <nawazishqadir.odesk@gmail.com>
2015-07-28 14:36:00 +0500

    Deploy command to clone git repo

--- script2.py
+++ script2.py
@@ -20,10 +20,9 @@ class Database(cmd.Cmd):
     __port = 3306
     __database = 'pythontest'
     
-	"""
-		Function that hits the MySQL database table orders.
-		returns dictionary for all the rows in the table.	
-	"""
+    """Function that hits the MySQL database table orders.
+        returns dictionary for all the rows in the table.	
+    """
     def get_dictionary(self, table='orders'):
         print "in get dictionary"
         import MySQLdb.cursors
@@ -32,8 +31,7 @@ class Database(cmd.Cmd):
         cursor.execute("SELECT * from {}".format(table))
         return cursor.fetchall()
 
-	"""
-		Invokes get_dictionary() for orders table
+    """Invokes get_dictionary() for orders table
         then converts to yaml
     """
     def dictionary_to_yaml(self):        
@@ -48,9 +46,9 @@ class Database(cmd.Cmd):
             d[k] = int(dic[k]) if type(dic[k]) == long else dic[k]
           orders_list.append(d)
 		
-		"""Checks if the folder ./out already exists.
-		If not creates one.
-		"""
+        """Checks if the folder ./out already exists.
+        If not creates one.
+        """
         directory = './out'		
         if not os.path.exists(directory):
             os.makedirs(directory)
@@ -62,16 +60,16 @@ class Database(cmd.Cmd):
     def do_build(self, line):
         self.dictionary_to_yaml()
 	
-	"""
-		Creates the git tags.
-		Checks if the tag with the same name is already created.
-		If not creates new one with the given name.
-		new_tag = 'tag_name'
-	"""
+    """
+        Creates the git tags.
+        Checks if the tag with the same name is already created.
+        If not creates new one with the given name.
+    """
     def do_release(self, line):
         repo = Repo('.')
         tags = repo.tags
-        new_tag = 'my_tag'
+        tag_name = raw_input("Please Enter a tag name to Create:")
+        new_tag = tag_name
         if new_tag not in tags:
             repo.create_tag(new_tag, message='V2.0')
             repo.git.push(tags=True)
@@ -84,29 +82,39 @@ class Database(cmd.Cmd):
 ##            tagref = tag.tag
 ##            print tagref 
             #repo.delete_tag(tagref)
-	"""
-	Function to deploy the code to github. It executes following commands
-	1- git init .
-	2- git add --all
-	3- git commit -m "Database Dump 1"
-	4- git push origin
-	"""	
-    def do_deploy(self, line):        
-        repo_dir = os.path.join(os.path.dirname(__file__), './')
-        repo = git.Repo.init(repo_dir)
-        print repo
-        repo = git.Repo(repo_dir, search_parent_directories=True)
-        print repo.git.status()
-        # add all files
-        print repo.git.add('*')
-        # commit        
-        print repo.git.commit(message='Database Dump 2' )
-        # now we are one commit ahead
-        print repo.git.status()
-        
-        #here is the new change
-        repo.git.push()
+    """
+    Function to deploy the code to github. It executes following commands
+    1- git init .
+    2- git add --all
+    3- git commit -m "Database Dump 1"
+    4- git push origin
+    """	
+##    def do_deploy(self, line):        
+##        repo_dir = os.path.join(os.path.dirname(__file__), './')
+##        repo = git.Repo.init(repo_dir)
+##        print repo
+##        repo = git.Repo(repo_dir, search_parent_directories=True)
+##        print repo.git.status()
+##        # add all files
+##        print repo.git.add('*')
+##        # commit        
+##        print repo.git.commit(message='Database Dump 2' )
+##        # now we are one commit ahead
+##        print repo.git.status()
+##        
+##        #here is the new change
+##        repo.git.push()
 
+    def do_deploy(self, line):        
+        git_url = raw_input("Please enter the clone URL:")
+        directory = git_url.rsplit('/',1)[1].split('.')[0]		
+        if not os.path.exists('./out'+directory):
+            os.makedirs('./out/'+directory)
+        repo_dir = os.path.join(os.path.dirname(__file__), './out/'+directory)
+        from git import Repo
+        Repo.clone_from(git_url, repo_dir)
+        print "Clone Successful"
+        
     def do_EOF(self, line):
         return True
 
