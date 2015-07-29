4e6096c687824020737b1529557c5312aa7370ac
Goran Angelkovski <goran.angelkovski@gmail.com>
2015-07-29 21:58:26 +0200

    Code clenup

--- script2.py
+++ script2.py
@@ -20,11 +20,10 @@ class Database(cmd.Cmd):
     __port = 3306
     __database = 'pythontest'
     
-    """Function that hits the MySQL database table orders.
+    """Function that hits the MySQL database table Orders.
         returns dictionary for all the rows in the table.	
     """
     def get_dictionary(self, table='orders'):
-        print "in get dictionary"
         import MySQLdb.cursors
         conn = MySQLdb.connect(host=self.__host, port=self.__port, user=self.__user, passwd=self.__password, db=self.__database, cursorclass=MySQLdb.cursors.DictCursor)
         cursor = conn.cursor()
@@ -38,28 +37,28 @@ class Database(cmd.Cmd):
         import yaml
         data = self.get_dictionary()    
         orders_list = []
-			
+
     #   Converting values with L(long) from db to int
         for dic in data:
           d = {}
           for k,v in dic.items():                
             d[k] = int(dic[k]) if type(dic[k]) == long else dic[k]
           orders_list.append(d)
-		
+
         """Checks if the folder ./out already exists.
         If not creates one.
         """
         directory = './out'		
         if not os.path.exists(directory):
             os.makedirs(directory)
-			
-        #Writing output to orders.yaml file
+
+        # Writing output to orders.yaml file
         with open(os.path.join(directory,'orders.yml'), 'w') as yaml_file:
             yaml_file.write( yaml.dump(orders_list, default_flow_style=False))
 
     def do_build(self, line):
         self.dictionary_to_yaml()
-	
+
     """
         Creates the git tags.
         Checks if the tag with the same name is already created.
@@ -77,10 +76,10 @@ class Database(cmd.Cmd):
             print "Tag already exists with the same name"
 
         """Deleting tags"""
-##        for tag in tags:
-##            print tag
-##            tagref = tag.tag
-##            print tagref 
+#        for tag in tags:
+#            print tag
+#            tagref = tag.tag
+#            print tagref
             #repo.delete_tag(tagref)
     """
     Function to deploy the code to github. It executes following commands
@@ -89,42 +88,42 @@ class Database(cmd.Cmd):
     3- git commit -m "Database Dump 1"
     4- git push origin
     """	
-##    def do_deploy(self, line):        
-##        repo_dir = os.path.join(os.path.dirname(__file__), './')
-##        repo = git.Repo.init(repo_dir)
-##        print repo
-##        repo = git.Repo(repo_dir, search_parent_directories=True)
-##        print repo.git.status()
-##        # add all files
-##        print repo.git.add('*')
-##        # commit        
-##        print repo.git.commit(message='Database Dump 2' )
-##        # now we are one commit ahead
-##        print repo.git.status()
-##        
-##        #here is the new change
-##        repo.git.push()
+#    def do_deploy(self, line):
+#        repo_dir = os.path.join(os.path.dirname(__file__), './')
+#        repo = git.Repo.init(repo_dir)
+#        print repo
+#        repo = git.Repo(repo_dir, search_parent_directories=True)
+#        print repo.git.status()
+#        # add all files
+#        print repo.git.add('*')
+#        # commit
+#        print repo.git.commit(message='Database Dump 2' )
+#        # now we are one commit ahead
+#        print repo.git.status()
+#
+#        #here is the new change
+#        repo.git.push()
 
     def do_deploy(self, line):        
-        git_url = raw_input("Please enter the clone URL:")
+        git_url = raw_input("Please enter the clone URL: ")
         directory = git_url.rsplit('/',1)[1].split('.')[0]		
         if not os.path.exists('./out'+directory):
             os.makedirs('./out/'+directory)
         repo_dir = os.path.join(os.path.dirname(__file__), './out/'+directory)
         from git import Repo
         Repo.clone_from(git_url, repo_dir)
+        # TODO rename
         print "Clone Successful"
         
     def do_EOF(self, line):
         return True
 
-    
 
 def main():
     d = Database()
-    #d.do_get_dictionary()
+    # d.do_get_dictionary()
     d.do_dictionary_to_yaml()
 
 if __name__ == '__main__':
-    #main()
+    # main()
     Database().cmdloop()
